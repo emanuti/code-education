2763b2a8ad7f2d93cba624bcdd18c9d0f0b8e53e
emanuti <emanu.ti@gmail.com>
2018-10-18 18:37:11 -0300

    alinhando botões de ação da lista

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -21,13 +21,19 @@
             <td>{{ $category->id }}</td>
             <td>{{ $category->name }}</td>
             <td>{{ $category->description }}</td>
-            <td>
-                <a href="{{ route('category.edit', ['id' => $category->id]) }}" class="btn">Edit</a> |
-                <form action="{{ route('category.destroy', ['id' => $category->id]) }}" method="POST">
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                    <button type="submit" class="btn btn-danger">Del</button>
-                </form>
+            <td class="col-md-3">
+                <div class="row">
+                    <div class="col-md-2">
+                        <a href="{{ route('category.edit', ['id' => $category->id]) }}" class="btn btn-sm">Edit</a>
+                    </div>
+                    <div class="col-md-2">
+                    <form action="{{ route('category.destroy', ['id' => $category->id]) }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button type="submit" class="btn btn-danger btn-sm">Del</button>
+                    </form>
+                    </div>
+                </div>
             </td>
         </tr>
         @endforeach
